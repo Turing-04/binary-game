b80aef46fb3bd5c9a405e95efe2714866709ab47
Turing-04 <marin.cornuot@gmail.com>
2022-11-22 15:01:04 +0100

    start of the Instruction part

--- src/instruction/instruction.js
+++ src/instruction/instruction.js
@@ -1,7 +1,5 @@
 import React from "react";
-import {
-    Instruction0, Instruction1, Instruction2, Instruction3, Instruction4
-} from "./instructionContent";
+import * as Instructions from "./instructionContent";
 
 
 class Instruction extends React.Component {
@@ -9,7 +7,7 @@ class Instruction extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            index: 1,
+            index: 0,
         };
     }
 
@@ -28,16 +26,52 @@ class Instruction extends React.Component {
 
     getContent() {
         switch (this.state.index) {
-            case 0:
-                return Instruction0();
-            case 1:
-                return Instruction1();
-            case 2:
-                return Instruction2();
-            case 3:
-                return Instruction3();
-            default:
-                return Instruction4();
+            case 0:return Instructions.Instruction0();
+            case 1:return Instructions.Instruction1();
+            case 2:return Instructions.Instruction2();
+            case 3:return Instructions.Instruction3();
+            case 4:return Instructions.Instruction4();
+            case 5:return Instructions.Instruction5();
+            case 6:return Instructions.Instruction6();
+            case 7:return Instructions.Instruction7();
+            case 8:return Instructions.Instruction8();
+            case 9:return Instructions.Instruction9();
+            case 10:return Instructions.Instruction10();
+            case 11:return Instructions.Instruction11();
+            case 12:return Instructions.Instruction12();
+            case 13:return Instructions.Instruction13();
+            case 14:return Instructions.Instruction14();
+            case 15:return Instructions.Instruction15();
+            case 16:return Instructions.Instruction16();
+            case 17:return Instructions.Instruction17();
+            case 18:return Instructions.Instruction18();
+            case 19:return Instructions.Instruction19();
+            case 20:return Instructions.Instruction20();
+            case 21:return Instructions.Instruction21();
+            case 22:return Instructions.Instruction22();
+            case 23:return Instructions.Instruction23();
+            case 24:return Instructions.Instruction24();
+            case 25:return Instructions.Instruction25();
+            case 26:return Instructions.Instruction26();
+            case 27:return Instructions.Instruction27();
+            case 28:return Instructions.Instruction28();
+            case 29:return Instructions.Instruction29();
+            case 30:return Instructions.Instruction30();
+            case 31:return Instructions.Instruction31();
+            case 32:return Instructions.Instruction32();
+            case 33:return Instructions.Instruction33();
+            case 34:return Instructions.Instruction34();
+            case 35:return Instructions.Instruction35();
+            case 36:return Instructions.Instruction36();
+            case 37:return Instructions.Instruction37();
+            case 38:return Instructions.Instruction38();
+            case 39:return Instructions.Instruction39();
+            case 40:return Instructions.Instruction40();
+            case 41:return Instructions.Instruction41();
+            case 42:return Instructions.Instruction42();
+            case 43:return Instructions.Instruction43();
+            case 44:return Instructions.Instruction44();
+            default:return Instructions.Instruction45();
         }
     }
 
--- src/instruction/instructionContent.js
+++ src/instruction/instructionContent.js
@@ -1,12 +1,16 @@
 import React from "react";
+import {getResults, addResults} from "../utils/firebase";
+import BubbleMessage from "../resources/message_bubble.png" 
+import Fingers from "../resources/finger_count.png"
+import Five from "../resources/five.png"
+import FiveHandwritten from "../resources/five_handwritten.png"
 
 export function Instruction0() {
     return (
         <div>
-            <h1>0</h1>
-            <p style={{width: 600}}>
-                Instruction 1
-            </p>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
         </div>
     );
 }
@@ -14,10 +18,12 @@ export function Instruction0() {
 export function Instruction1() {
     return (
         <div>
-            <h1>1</h1>
-            <p style={{width: 600}}>
-                Instruction 1
-            </p>
+            <h1>You can say the name of the number, <br/></h1>
+            <h1 style={{textAlign:'left'}}>For instance for 5 : </h1>
+            <img
+            src={BubbleMessage}
+            alt="message_bubble"
+            />
         </div>
     );
 }
@@ -25,10 +31,12 @@ export function Instruction1() {
 export function Instruction2() {
     return (
         <div>
-            <h1>2</h1>
-            <p style={{width: 600}}>
-                Instruction 1
-            </p>
+            <h1>You can also hold up your fingers to show <br/>
+            a number, like this:</h1>
+            <img
+            src={Fingers}
+            alt="fingers_count"
+            />
         </div>
     );
 }
@@ -36,10 +44,13 @@ export function Instruction2() {
 export function Instruction3() {
     return (
         <div>
-            <h1>3</h1>
-            <p style={{width: 600}}>
-                Instruction 1
-            </p>
+            <h1>You can also write the symbol of the <br/>
+            number, as you have learnt in school:</h1>
+            <img 
+            class= "center"
+            src={Five}
+            alt="five_digit"
+            />
         </div>
     );
 }
@@ -47,10 +58,135 @@ export function Instruction3() {
 export function Instruction4() {
     return (
         <div>
-            <h1>4</h1>
-            <p style={{width: 600}}>
-                Instruction 1
-            </p>
+            <h1>You can even write down the name of the <br/>
+            number like this:</h1>
+            <img 
+            class = "center"
+            src={FiveHandwritten}
+            alt="handwritten_five"
+            />
         </div>
     );
 }
+
+
+export function Instruction5() {
+    return (
+        <div>
+            <h1>You can even write down the name of the <br/>
+            number like this:</h1>
+            <img 
+            class = "center"
+            src={FiveHandwritten}
+            alt="handwritten_five"
+            />
+        </div>
+    );
+}
+
+export function Instruction6() {
+    return (
+        <div>
+            <h1>Can we represent the numbers differently ? <br/><br/>
+             What if we can't speak, hold up our fingers, or <br/>
+             write something down ?<br/><br/>
+             How would we show somebody what number we are thinking of ?</h1>
+        </div>
+    );
+}export function Instruction7() {
+    return (
+        <div>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
+        </div>
+    );
+}export function Instruction8() {
+    return (
+        <div>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
+        </div>
+    );
+}export function Instruction9() {
+    return (
+        <div>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
+        </div>
+    );
+}export function Instruction10() {
+    return (
+        <div>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
+        </div>
+    );
+}export function Instruction11() {
+    return (
+        <div>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
+        </div>
+    );
+}export function Instruction12() {
+    return (
+        <div>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
+        </div>
+    );
+}export function Instruction13() {
+    return (
+        <div>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
+        </div>
+    );
+}export function Instruction14() {
+    return (
+        <div>
+            <h1>Let's start with a question: <br/>
+             How do we represent digits (the number 0 to 9) <br/>
+             in day to day life ?</h1>
+        </div>
+    );
+}
+
+export function Instruction15(){}
+export function Instruction16(){}
+export function Instruction17(){}
+export function Instruction18(){}
+export function Instruction19(){}
+export function Instruction20(){}
+export function Instruction21(){}
+export function Instruction22(){}
+export function Instruction23(){}
+export function Instruction24(){}
+export function Instruction25(){}
+export function Instruction26(){}
+export function Instruction27(){}
+export function Instruction28(){}
+export function Instruction29(){}
+export function Instruction30(){}
+export function Instruction31(){}
+export function Instruction32(){}
+export function Instruction33(){}
+export function Instruction34(){}
+export function Instruction35(){}
+export function Instruction36(){}
+export function Instruction37(){}
+export function Instruction38(){}
+export function Instruction39(){}
+export function Instruction40(){}
+export function Instruction41(){}
+export function Instruction42(){}
+export function Instruction43(){}
+export function Instruction44(){}
+export function Instruction45(){}
