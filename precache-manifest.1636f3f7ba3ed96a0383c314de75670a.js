b7360080c8ba02b6400551ee51fbd6d9befe49d4
Zéphyr <antoine.familleroger@gmail.com>
2022-11-27 12:18:42 +0100

    New build

---
+++ precache-manifest.1636f3f7ba3ed96a0383c314de75670a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b08b831ebff6bf990157",
+    "url": "./static/css/main.829910f9.chunk.css"
+  },
+  {
+    "revision": "b08b831ebff6bf990157",
+    "url": "./static/js/main.b08b831e.chunk.js"
+  },
+  {
+    "revision": "c0d850e1296c0a3903a2",
+    "url": "./static/js/1.c0d850e1.chunk.js"
+  },
+  {
+    "revision": "6a4f9df79ae64c2c94ba",
+    "url": "./static/js/2.6a4f9df7.chunk.js"
+  },
+  {
+    "revision": "6f9080bcf9f3519144ae",
+    "url": "./static/js/runtime~main.6f9080bc.js"
+  },
+  {
+    "revision": "be92221869f663f16b3b2e3a7aba17df",
+    "url": "./static/media/smiley_face.be922218.png"
+  },
+  {
+    "revision": "bc3bc8f65425be4d7f83e8dc1fc5c3c0",
+    "url": "./static/media/finger_count.bc3bc8f6.png"
+  },
+  {
+    "revision": "fe2d4d3e34212ab6f748b2e138afb621",
+    "url": "./static/media/instruction_40.fe2d4d3e.png"
+  },
+  {
+    "revision": "6ea770324bb1a75aa3abf1c28001cafe",
+    "url": "./static/media/instruction_41.6ea77032.png"
+  },
+  {
+    "revision": "35a6433da2b44edb221c5c4fd46e5204",
+    "url": "./static/media/instruction_44.35a6433d.png"
+  },
+  {
+    "revision": "18b78ce2cc1d046bff9653022f83abde",
+    "url": "./static/media/instruction_45.18b78ce2.png"
+  },
+  {
+    "revision": "1c9e4f8520ab8effa6a178d5361463a5",
+    "url": "./static/media/message_bubble.1c9e4f85.png"
+  },
+  {
+    "revision": "244283da2afae3162ea901b03f756ff3",
+    "url": "./static/media/five_handwritten.244283da.png"
+  },
+  {
+    "revision": "c409a3ee8fc42f976afd4670a1a4279f",
+    "url": "./static/media/light_off.c409a3ee.png"
+  },
+  {
+    "revision": "a4e3f8b5ca35d81078f3fb980d559d7f",
+    "url": "./static/media/light_on.a4e3f8b5.png"
+  },
+  {
+    "revision": "2ec811f578e9582f7af8b9bf3d1e78f8",
+    "url": "./static/media/message_bubble_de.2ec811f5.png"
+  },
+  {
+    "revision": "57393750011ac5aeaa1f7b457b02e27d",
+    "url": "./static/media/five_handwritten_de.57393750.png"
+  },
+  {
+    "revision": "f5961011263900a4bd5ca70a1c076eb3",
+    "url": "./index.html"
+  }
+];
